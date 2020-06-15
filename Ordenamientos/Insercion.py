ea06822042c85d298de7369cff82b49af959081b
Cristian Ortega <emanuel.ortega@hotmail.com>
2020-06-14 20:58:02 -0500

    Inserccion Completed

--- Ordenamientos/Insercion.py
+++ Ordenamientos/Insercion.py
@@ -7,4 +7,11 @@ class Insercion:
     @staticmethod    
     def sort (lista):
         start_time = time.time()
+        for i in range(1,len(lista)):
+            aux = lista[i]
+            indice = i
+            while indice > 0 and lista[indice -1] > aux:
+                lista[indice] = lista[indice-1]
+                indice = indice - 1
+            lista[indice] = aux
         return ((time.time() - start_time)*1000000)
