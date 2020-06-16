8199911f168de8c97b7ad2b5ebbc5add7de8cae8
Cristian Ortega <emanuel.ortega@hotmail.com>
2020-06-15 20:03:56 -0500

    Timer fixed

--- Ordenamientos/Ordenamientos.py
+++ Ordenamientos/Ordenamientos.py
@@ -10,43 +10,76 @@ tiempo_seleccion = 0.0
 tiempo_insercion = 0.0
 tiempo_quicksort = 0.0
 
-lista = [random.randrange(1, 100, 1) for i in range(1000)]
-
 print('El resultado con: 1000 datos:')
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(1000)]
 print('El tiempo de Burbuja es: ',Burbuja.sort(lista),' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(1000)]
 print('El tiempo de Selección es: ',Seleccion.sort(lista),' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(1000)]
 start_time = time.time()
 Quicksort.sort(lista)
 tiempo = (time.time() - start_time)*1000000
 print('El tiempo de Quicksort es: ',tiempo,' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(1000)]
 print('El tiempo de Inserción es: ',Insercion.sort(lista),' micro segundos')
 print(' ')
 print(' ')
 print(' ')
 
-lista = [random.randrange(1, 10000, 1) for i in range(10000)]
+
+
 
 print('El resultado con: 10000 datos:')
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(10000)]
 print('El tiempo de Burbuja es: ',Burbuja.sort(lista),' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(10000)]
 print('El tiempo de Selección es: ',Seleccion.sort(lista),' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(10000)]
 start_time = time.time()
 Quicksort.sort(lista)
 tiempo = (time.time() - start_time)*1000000
 print('El tiempo de Quicksort es: ',tiempo,' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(10000)]
 print('El tiempo de Inserción es: ',Insercion.sort(lista),' micro segundos')
 print(' ')
 print(' ')
 print(' ')
 
-lista = [random.randrange(1, 20000, 1) for i in range(20000)]
+
+
 
 print('El resultado con: 20000 datos:')
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(20000)]
 print('El tiempo de Burbuja es: ',Burbuja.sort(lista),' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(20000)]
 print('El tiempo de Selección es: ',Seleccion.sort(lista),' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(20000)]
 start_time = time.time()
 Quicksort.sort(lista)
 tiempo = (time.time() - start_time)*1000000
 print('El tiempo de Quicksort es: ',tiempo,' micro segundos')
+
+random.seed(3)
+lista = [random.randrange(1, 100, 1) for i in range(20000)]
 print('El tiempo de Inserción es: ',Insercion.sort(lista),' micro segundos')
 print(' ')
 print(' ')
